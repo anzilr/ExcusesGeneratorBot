5b95538d31967172765d45e17d27c0f05e988660
Anzil R <anzilraheem@gmail.com>
2023-04-10 23:56:42 +0300

    Removed unwanted strings

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "2039956077:AAGt83sWf5th4CfEIhIrwixqf7GnC2JHsWI")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 START_TEXT = '<b>Hi! I am 🤖 ExcuseGeneratorBot. \n\nI can give you 🧠 AI generated excuses for your mess-ups 😜.\n\nSend "/excuse" to start</b>'
