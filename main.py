f720b02bb83e9bbadfcb67b51b212db4e4fb2652
Anzil R <anzilraheem@gmail.com>
2023-04-10 23:54:49 +0300

    Moved message processing to message_handler.py

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
 from fastapi import FastAPI, Request
-from api import Generator
-from config import START_TEXT, WRONG_INPUT
-from message_handler import SendMessage, MessageParser
+from message_handler import MessageParser, conversation
+
 
 app = FastAPI()
 
 
-@app.post("/")
+@app.post('/')
 async def index(req: Request):
     msg = await req.json()
-
     chat_id, text = await MessageParser(msg)
-    # print(text)
-    try:
-        if text == "/start":
-            await SendMessage(chat_id, text=START_TEXT)
-
-        else:
-            try:
-                messup, professionalism, target = text.split("|")
-                response = await Generator(messup, professionalism, target)
-                await SendMessage(chat_id, response)
-
-            except ValueError:
-                await SendMessage(chat_id, text=WRONG_INPUT)
-    except:
-        pass
+    await conversation(chat_id, text)
 
 
 @app.get("/")
